0495d9fca54f7c0f0e849b3ce8373fa67796ecb4
reeseoo <reesejmorris@gmail.com>
2017-02-09 20:11:26 +0000

    Removing create react app bulk

--- src/App.js
+++ src/App.js
@@ -6,13 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          Trying to get on Heroku.
-        </p>
       </div>
     );
   }
